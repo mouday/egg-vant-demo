35dcf7dcd2e1958413904c0dc41e82d47f6d4efd
pengshiyu <1940607002@qq.com>
2020-03-19 20:57:50 +0800

    add

--- vant-client/src/App.vue
+++ vant-client/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    
+
     <van-tabbar route>
       <van-tabbar-item replace to="/" icon="home-o">主页</van-tabbar-item>
       <van-tabbar-item replace to="/add" icon="plus">发布</van-tabbar-item>
     </van-tabbar>
-
   </div>
 </template>
 
@@ -18,11 +17,10 @@ export default {
 
   components: {
     [Tabbar.name]: Tabbar,
-    [TabbarItem.name]: TabbarItem,
+    [TabbarItem.name]: TabbarItem
   }
 };
 </script>
 
 <style>
-
 </style>
